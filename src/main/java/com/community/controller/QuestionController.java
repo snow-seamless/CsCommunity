ac3cad2769c629adbd626dfd48ad829d3faf4120
name = HeChunj <1098648301@qq.com>
2022-05-14 22:48:26 +0800

    修复QuestionController

--- src/main/java/com/community/controller/QuestionController.java
+++ src/main/java/com/community/controller/QuestionController.java
@@ -52,7 +52,7 @@ public class QuestionController {
         model.addAttribute("question", questionDTO);
         model.addAttribute("comments", commentDTOList);
         model.addAttribute("relatedQuestions", relatedQuestions);
-        return "/question";
+        return "question";
     }
 
 }
